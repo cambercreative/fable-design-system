ceb84c2ae48fcb7b09121e226864c415452fcd5c
Camber <13381522+cambercreative@users.noreply.github.com>
2025-04-10 14:02:25 -0400

    Fix: Update page metadata to use SVG favicon

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Fable Design System',
   description: "Visual documentation of Fable's design language",
+  icons: {
+    icon: '/favicon.svg',
+  }
 }
 
 export default function Home() {
