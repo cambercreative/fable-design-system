e65ed939e8554e52882ddb503d2c82d4848ade2c
Camber <13381522+cambercreative@users.noreply.github.com>
2025-04-10 14:05:42 -0400

    Fix: Remove explicit favicon from metadata

--- app/metadata.tsx
+++ app/metadata.tsx
@@ -2,8 +2,5 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Fable Design System',
-  description: "Visual documentation of Fable's design language",
-  icons: {
-    icon: '/favicon.svg',
-  }
+  description: "Visual documentation of Fable's design language"
 }
