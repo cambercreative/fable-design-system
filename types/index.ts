86aef8546ff0f5510290ef2e97f87042e24d2366
Camber <13381522+cambercreative@users.noreply.github.com>
2025-04-11 17:15:06 -0400

    Create centralized types directory with index file

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Fable Design System - Type Definitions
+ * 
+ * This file exports all type definitions used throughout the design system.
+ * It serves as a centralized entry point for importing types.
+ */
+
+export * from './components';
+export * from './shared';
